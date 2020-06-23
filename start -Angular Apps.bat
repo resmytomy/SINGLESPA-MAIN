6c1cdfffc2e7014330f46b147e3675a49895a96e
Tomy <503188394@logon.ds.ge.com>
2020-06-23 11:36:49 +0530

    batch files

---
+++ start -Angular Apps.bat
@@ -0,0 +1,15 @@
+
+start /min cmd /c " cd AngularAppForCMD && npm start"
+start /min cmd /c " cd AngularAppForCPU && npm start"
+start /min cmd /c " cd AngularAppForFileEdit && npm start"
+start /min cmd /c " cd AngularAppForHardware && npm start"
+start /min cmd /c " cd AngularAppForTree && npm start"
+start /min cmd /c " cd LoggerApp && npm start"
+start /min cmd /c " cd NavigationBar && npm start"
+start /min cmd /c " cd SingleSPARoot && npm start"
+start /min cmd /c " cd AddUserApp && npm start"
+start /min cmd /c " cd LoginApp && npm start"
+
+
+
+
---
+++ start-nodeApps.bat
@@ -0,0 +1,9 @@
+start node NodeForTree/server.js
+start node NodeForCMD/server.js
+start node NodeForCpuDetails/server.js
+start node NodeForFileEdit/server.js
+start node NodeForHardware/server.js
+start node JWTMiddleware/server.js
+
+
+
